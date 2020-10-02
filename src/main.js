b87ce47fb693d7572645fc949a13a8c2d3be3b29
Hailot <mightylordfe@gmail.com>
2020-10-02 05:35:42 +0200

    socket io

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  },
+  options: { path: "/my-app/" } //Optional options
+}))
+
+
 new Vue({
   router,
   store,
